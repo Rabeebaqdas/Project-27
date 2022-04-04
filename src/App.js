e0fe69bc0603936df3b6ad954952f9d07046cff8
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-04 12:41:22 +0500

    this is the fiveteenth project form 50 project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React,{useState} from "react";
+import React,{useEffect} from "react";
 import './App.css';
+
 function App() {
- const [count,setCount] = useState(false);
+useEffect(()=>{
+  const counter = document.querySelectorAll('.counter');
+
+    counter.forEach(count=>{
+      count.innerHTML = '0'
+      
+      const updateCounter = () => {
+        const target = count.getAttribute('data-target')
+        const c = +count.innerText
+        const increment = target / 200
+        if(c < target) {
+          count.innerText = `${Math.ceil(c + increment)}`
+          setTimeout(updateCounter,1)
+        }else{
+          count.innerText = target
+        }
+      }
+      updateCounter()
+    })
+    
+},[])
+
   return (
 
-      <div>
-        <nav className={count ? "active" : ""}>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Works</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-
-        </ul>
-        <button className="icon" onClick={()=>setCount(!count)}>
-          <div className="line line1"></div>
-          <div className="line line2"></div>
+    <div className="main" style={{display:"flex"}}>
+    <div className="counter-container">
+        <i className="fab fa-twitter fa-3x"></i>
+        <div className="counter" data-target="12000"></div>
+        <span>Twitter followers</span>
         
-        </button>
-        </nav>
-
-      </div>
-  );
+    </div>
+    <div className="counter-container">
+        <i className="fab fa-youtube fa-3x"></i>
+        <div className="counter" data-target="5000"></div>
+        <span>Youtube subscribers</span>
+        
+    </div>
+    <div className="counter-container">
+        <i className="fab fa-facebook fa-3x"></i>
+        <div className="counter" data-target="7500"></div>
+        <span>Facebook fans</span>
+        
+    </div>
+</div>  
+);
 }
 
 export default App;
