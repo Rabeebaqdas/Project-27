cdd5431f51762b464e16991d5db7b7ba487857be
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-05 15:10:26 +0500

    this is the ninteenth project from 50 project

--- src/App.js
+++ src/App.js
@@ -2,76 +2,71 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-  const [count,setCount] = useState({count:0});
-
-  const handleLeft = () => {
-    if(count.count == 0 ){
-      setCount({
-        count:3
-      })
-      return;
-    }
-
-    setCount({
-      count:count.count-1
-    })
  
+  const body = document.body;
+  function color () {
+    body.style.backgroundColor = theme?body.style.backgroundColor="#111":body.style.backgroundColor="white"
+  }
+const [theme,setTheme] = useState(false)
+const [min,setMin] = useState('')
+const [hours,setHours] = useState('')
+const [ampm,setampm] = useState('')
+const[day,setDay] = useState({
+  day:'',
+  month:''
+})
+const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+const months = ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"];
+useEffect(()=>{
+  const scale = (num,in_min,in_max,out_min,out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+  const hourEL= document.querySelector('.hour');
+  const minEL= document.querySelector('.minute');
+  const secEL= document.querySelector('.second');
+  const timeEl= document.querySelector('.time');
+  const dateEL= document.querySelector('.date');
+  function setTime(){
+    const time = new Date()
+    const month = time.getMonth()
+    const day = time.getDay()
+    const hours = time.getHours()
+    const hourForClock = hours % 12
+    const mins = time.getMinutes()
+    const sec = time.getSeconds()
+   setDay({
+     day:day,
+     month:month
+   })
+ 
+    setampm(hours)
+    setMin(mins)
+    setHours(hourForClock)
+    hourEL.style.transform = `translate(-50%, -100%) rotate(${scale(hourForClock,0,11,0,360)}deg)`
+    minEL.style.transform = `translate(-50%, -100%) rotate(${scale(mins,0,59,0,360)}deg)`
+    secEL.style.transform = `translate(-50%, -100%) rotate(${scale(sec,0,59,0,360)}deg)`
   }
 
-  const handleRight = () => {
+  setInterval(setTime,1000)
+},[])
 
-    if(count.count == 3 ){
-      setCount({
-        count:0
-      })
-      return;
-    }
-    setCount({
-      count:count.count+1
-    })
- 
-   
-  }
 
-  useEffect(()=>{
-    const body = document.body;
-    const slides = document.querySelectorAll('.slide')
-    const bg = () =>{
-      body.style.backgroundImage = slides[count.count].style.backgroundImage;
-    }
-    bg();
-
-  },[count.count])
-
- 
-
+color()
   return (
     <>
-      <div className="slider-container">
-        <div
-          className={`slide ${count.count==0 ? "active" : ""}`}
-          style={{ backgroundImage: `url(https://images.unsplash.com/photo-1649004542972-9a70c13d875f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=963&q=80)` }}
-        ></div>
-        <div
-          className={`slide ${count.count==1 ? "active" : ""}`}
-          style={{ backgroundImage: `url(https://images.unsplash.com/photo-1649011525614-6a97c8f8889e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)` }}
-        ></div>
-        <div
-          className={`slide ${count.count==2 ? "active" : ""}`}
-          style={{ backgroundImage: `url(https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)` }}
-        ></div>
-        <div
-          className={`slide ${count.count==3 ? "active" : ""}`}
-          style={{ backgroundImage: `url(https://images.unsplash.com/photo-1648737154547-b0dfd281c51e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)` }}
-        ></div>
-        <button className="arrow left-arrow" id="left" onClick={handleLeft}>
-            <i className="fas fa-arrow-left"></i>
-        </button>
-        <button className="arrow right-arrow" id="right" onClick={handleRight}>
-            <i className="fas fa-arrow-right"></i>
-        </button>
-      </div>
-    </>
+    <button className="toggle" style={{backgroundColor:theme ? "white" : "black", color:theme ? "black" : "white"} } onClick={()=>setTheme(!theme)}>{theme ? "Light mode" : "Dark mode"}</button>
+    <div className={`clock-container ${theme ? "dark" : ""} `}>
+        <div className="clock">
+            <div className="needle hour"></div>
+            <div className="needle minute"></div>
+            <div className="needle second"></div>
+            <div className="center-point"></div>
+
+        </div>
+        <div className="time" style={{color:theme ? "white" :"black"}}>{`${hours}:${min < 10 ? `0${min}` : min}`}{`${ampm >= 12 ? " PM" : " AM" }`}</div>
+        <div className="date">{`${days[day.day]}`}, {`${months[day.month]}`}<span className="circle" style={{backgroundColor:theme ? "white" :"black",color:theme ? "black" :"white"}} >{`${day.day}`}</span></div>
+    </div>
+</>
   );
 }
 
