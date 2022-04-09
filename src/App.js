4b83e20282b398720c7859903671216792db02af
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-09 12:07:24 +0500

    this is the 27th project from 50 project

--- src/App.js
+++ src/App.js
@@ -1,136 +1,51 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
+import React,{useRef} from 'react';
+
 
 
 function App() {
 
-  const [apiData, setapiData] = useState();
-  const [filterapi, setfilterapi] = useState();
-  const [state, setState] = useState({
-    value: '',
-    show: ''
-  });
+  const button = useRef(null);
+  const toasts = useRef(null);
 
+  const messages = [
+    "Message One",
+    "Message Two",
+    "Message Three",
+    "Message Four",
+    "Message Five"
+  ]
 
-  const handleChange = (e) => {
-    setState({ value: e.target.value })
-  }
+  const types = [
+    "info",
+    "success",
+    "error"
+  ]
 
-  const submit = (e) => {
-    e.preventDefault()
-    setState({ show: state.value })
-    console.log(state.value)
+const createNotification = (message = null , type = null) => {
+  const notif = document.createElement("div");
+  notif.classList.add("toast")
+  notif.classList.add(type ? type : getRandomType())
+  notif.innerText = message ? message: getRandomMessage();
+  toasts.current.appendChild(notif);
+  setTimeout(()=>{
+    notif.remove()
+  },3000)
+}
 
-  }
-
-
-  const Image_path = 'https://image.tmdb.org/t/p/w1280';
-  const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=fbffc89359898a66e6ba200f3e92b611&query="';
-
-  useEffect(() => {
-
-    const getProducts = async () => {
-      try {
-      
-        const res = await axios.get("https://api.themoviedb.org/3/discover/movie?sort_bypopularity.desc&api_key=fbffc89359898a66e6ba200f3e92b611");
-        setapiData(res.data)
-      
-      
-      }
-      catch (err) {
-        console.log(err)
-      }
-    };
-    getProducts();
-  }, [])
-
-  useEffect(() => {
-
-    const filteredProduct = async () => {
-      try {
-      
-        const res = await axios.get(SEARCH_URL + state.show);
-  
-        setfilterapi(res.data)
-      }
-      catch (err) {
-        console.log(err)
-      }
-    };
-    filteredProduct();
-  }, [state.show])
-
+const getRandomMessage = () => {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+const getRandomType = () => {
+  return types[Math.floor(Math.random() * types.length)]
+}
 
   return (
-    <>
-      <header>
-        <form id="form">
-          <input type="search" id="search" className='search' placeholder='search' value={state.value} onChange={(e) => handleChange(e)} />
-          <button type="submit" onClick={(e) => submit(e)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </button>
-        </form>
-      </header>
-      <main id="main">
-
-        {
-          state.show && filterapi.results != null ?
-            filterapi
-              ?
-              filterapi.results.map((apiObj) => {
-      
-                return (
-                  <div className="movie">
-                    <img src={Image_path + apiObj.backdrop_path}
-                      alt="" />
-                    <div className="movie-info">
-                      <h3>{apiObj.original_title}</h3>
-                      <span className='green'>{apiObj.vote_average}</span>
-                    </div>
-                    <div className="overview">
-                      <h3>overview</h3>
-                      {apiObj.overview}
-                    </div>
-                  </div>
-                )
-              })
-              :
-              <h1>
-                data is comming
-              </h1>
-            :
-            apiData
-              ?
-              apiData.results.map((apiObj) => {
-              
-                return (
-                  <div className="movie">
-                    <img src={Image_path + apiObj.backdrop_path}
-                      alt="" />
-                    <div className="movie-info">
-                      <h3>{apiObj.original_title}</h3>
-                      <span className={apiObj.vote_average > 7 ? 'green' : apiObj.vote_average > 4 ? "orange" : "red"}>{apiObj.vote_average}</span>
-                    </div>
-                    <div className="overview">
-                      <h3>overview</h3>
-                      {apiObj.overview}
-                    </div>
-                  </div>
-                )
-              })
-              :
-              <h1>
-                data is comming
-              </h1>
-        }
-
-
-
-      </main>
-    </>
+    <div>
+        <div id="toasts" ref={toasts}>
+        </div>
+        <button className="btn" id="button" ref={button} onClick={()=>createNotification()}>Show Notification</button>
+    </div>
   )
 }
 
